0369c89fc001c8d79a0d52e46cdd586377ec387f
MahboobGeeky <mahboob9184@gmail.com>
2026-03-17 22:31:45 +0530

    done

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -22,18 +22,17 @@ const router = createBrowserRouter([
         path: "about",
         element: <About/>
       },
-      {
-        path: "contact",
-        element: <Contact/>
-      },
-      {
-        path: "user",
-        element: <User/>
-      }
+      // {
+      //   path: "contact",
+      //   element: <Contact/>
+      // },
+      // {
+      //   path: "user",
+      //   element: <User/>
+      // }
     ]
   }
 ])
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
