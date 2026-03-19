81bfdc29d92b1dfe0ecb96dd607bbedafd7d1802
MahboobGeeky <mahboob9184@gmail.com>
2026-03-19 09:12:07 +0530

    github

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -3,6 +3,7 @@ import { useLoaderData } from 'react-router-dom'
 
 function Github() {
   const data = useLoaderData()
+
   // const [data, setData] = useState([])
   // useEffect(() => {
   //   fetch('https://api.github.com/users/MahboobGeeky')
@@ -15,7 +16,7 @@ function Github() {
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
     <img src={data.avatar_url} alt="Git picture" width={300} />
-    <p className='font-bold'>{data.bio}</p>
+    <p className=' text-justify align-middle font-bold'>{data.bio}</p>
     </div>
   )
 }
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -8,7 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
-import Github from "./components/Github/Github.jsx";
+import Github, { githubInfoLoader } from "./components/Github/Github.jsx";
 import { useLoaderData } from "react-router-dom";
 
 // const router = createBrowserRouter([
@@ -41,13 +41,9 @@ const router = createBrowserRouter(
     <Route path='/' element={<Layout/>}>
       <Route path='' element={<Home/>} />
       <Route path='about' element={<About/>} />
-      <Route path='home' element={<Home/>} />
       <Route path='contact' element={<Contact/>} />
       <Route path='user/:userid' element={<User/>} />
-      <Route
-        loader={githubInfoLoader}
-        path="github" 
-        element={<Github/>} />
+      <Route path="github" element={<Github/>} loader={githubInfoLoader}/>
     </Route>
   )
 )
