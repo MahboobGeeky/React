ccd98d3d2aa49a1d31bf9f2db4e6c8795d93972f
MahboobGeeky <mahboob9184@gmail.com>
2026-05-05 20:08:26 +0530

    footer and header

---
+++ 12-mega-blog/src/components/container/Container.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Children } from 'react'
+
+function Container() {
+    return (
+        <div className='w-full max-w-7xl max-auto'>{children}</div>
+    )
+}
+
+export default Container
