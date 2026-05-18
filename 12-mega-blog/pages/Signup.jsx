643b54479f412938086bd1438f70a7ea86861e4c
MahboobGeeky <mahboob9184@gmail.com>
2026-05-18 20:10:23 +0530

    react completed BOOM...

---
+++ 12-mega-blog/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Signup as SignupComponet } from '../src/components'
+function Signup() {
+    return (
+        <div className='py-8'>
+            <SignupComponet/>
+        </div>  
+    )
+}
+
+export default Signup
